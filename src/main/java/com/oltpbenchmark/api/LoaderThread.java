13e1ed3860bbd47c7a57067c867613935fd79d8c
Maksim Zinal <mzinal@ru.ibm.com>
2024-01-10 10:44:32 +0300

    LoaderThread.java, as sent to customer at 20.12.2023

--- src/main/java/com/oltpbenchmark/api/LoaderThread.java
+++ src/main/java/com/oltpbenchmark/api/LoaderThread.java
@@ -1,20 +1,3 @@
-/*
- * Copyright 2020 by OLTPBenchmark Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package com.oltpbenchmark.api;
 
 import org.slf4j.Logger;
@@ -23,11 +6,6 @@ import org.slf4j.LoggerFactory;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-/**
- * A LoaderThread is responsible for loading some portion of a
- * benchmark's database.
- * Note that each LoaderThread has its own database Connection handle.
- */
 public abstract class LoaderThread implements Runnable {
 
     private static final Logger LOG = LoggerFactory.getLogger(LoaderThread.class);
@@ -49,25 +27,17 @@ public abstract class LoaderThread implements Runnable {
             LOG.error(msg, next_ex);
             throw new RuntimeException(ex);
         } finally {
+            benchmarkModule.returnConnection();
             afterLoad();
         }
     }
 
-    /**
-     * This is the method that each LoaderThread has to implement
-     *
-     * @param conn
-     * @throws SQLException
-     */
     public abstract void load(Connection conn) throws SQLException;
 
     public void beforeLoad() {
-        // useful for implementing waits for countdown latches, this ensures we open the connection right before its used to avoid stale connections
     }
 
     public void afterLoad() {
-        // useful for counting down latches
     }
-
 
 }
